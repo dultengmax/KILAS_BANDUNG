65a8a2552dc17d1dc805238b3ddea5f3907f2ad3
dultengmax <aziznasirudin@gmail.com>
2026-01-02 06:16:24 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**': ['./prisma/src/generated/prisma/*.so.node'],
+    },
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
